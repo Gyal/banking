e845543ffc83f1ecdeb6e117af2edc978ef2059b
Nirogeni <v.nirogeni@gmail.com>
2015-02-18 00:30:01 +0100

    Base Project Banking

---
+++ src/test/java/fr/iut/lpcsid/bankingProject/BankingApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.iut.lpcsid.bankingProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BankingApplication.class)
+public class BankingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
