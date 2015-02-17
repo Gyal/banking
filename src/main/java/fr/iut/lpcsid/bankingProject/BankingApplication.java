c7281746aadeb83921bb421c580925a63de75f88
Joinville <joinville.juliana@gmail.com>
2015-02-18 00:48:33 +0100

    test

--- src/main/java/fr/iut/lpcsid/bankingProject/BankingApplication.java
+++ src/main/java/fr/iut/lpcsid/bankingProject/BankingApplication.java
@@ -8,6 +8,6 @@ public class BankingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankingApplication.class, args);
-        System.out.print("test niiro");
+        System.out.print("test nana ");
     }
 }
