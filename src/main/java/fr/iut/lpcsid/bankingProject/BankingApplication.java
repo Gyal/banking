8f180ccbd2d3af0921b00102e71383771396ce8d
Nirogeni <v.nirogeni@gmail.com>
2015-02-18 00:45:19 +0100

    Modif Niiro

--- src/main/java/fr/iut/lpcsid/bankingProject/BankingApplication.java
+++ src/main/java/fr/iut/lpcsid/bankingProject/BankingApplication.java
@@ -8,6 +8,6 @@ public class BankingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankingApplication.class, args);
-        System.out.print("tets");
+        System.out.print("test niiro");
     }
 }
